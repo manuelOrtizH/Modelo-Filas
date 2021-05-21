e8c3b35c7775028c506f51a3c26471aa07d8ed01
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-05-21 00:32:23 -0500

    Updated MG1

--- modelos_filas_project/simulator/scripts/mg1.py
+++ modelos_filas_project/simulator/scripts/mg1.py
@@ -1,23 +1,26 @@
 import math
 class MG1(object):
-    def __init__(self, l, time, n):
+    def __init__(self, l, miu, n):
         #Constructor para inicializar todos los atributos
         self.l = l
-        self.po = calculatePo()
-        self.pn = calculatePn(n)
-        self.miu = (1/time)*60
-        self.variance = math.pow(1/(self.miu*self.miu),2)
-        self.p = calculateP()
-        self.lq = calculateLq()
-        self.wq = calculateWq()
-        self.w = calculateW()
-        self.L = calculateL()
+        self.miu = miu
+        self.variance = 0 #math.pow(1/(self.miu*self.miu),2)
+        self.p = self.calculateP()
+        self.po = self.calculatePo()
+        self.pn = self.calculatePn(n)
+        self.lq = self.calculateLq()
+        self.wq = self.calculateWq()
+        self.w = self.calculateW()
+        self.L = self.calculateL()
 
-    def calculatePn(self, n):
-        return [math.pow(self.p, i+1) * self.po for i in range(n)]
+    def calculateP(self):
+        return self.l/self.miu
 
     def calculatePo(self):
         return 1 - self.p
+
+    def calculatePn(self, n):
+        return [math.pow(self.p, i+1) * self.po for i in range(n)]
 
     def calculateLq(self):
         return (((self.l * self.l) * self.variance + (self.p * self.p)) / (2*(1-self.p)) )
@@ -28,14 +31,12 @@ class MG1(object):
     def calculateW(self):
         return self.wq + (1/self.miu)
 
-    def calculateP(self):
-        return self.l/self.miu
-
     def calculateL(self):
         return self.l * self.w
 
     def __str__(self):
-        return f'n: {self.n}, '
+        return f'lq: {self.lq}, L: {self.L}, w: {self.w}, wq: {self.wq}, Po: {self.po}, p: {self.p}'
         #Imprimir todo los atributos
 
-m = MG1(9, 5, 1)
+m = MG1(0.05, 0.066, 1)
+print(m)
